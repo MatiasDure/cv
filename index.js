dd3c84d60d481914f2036611b038b4ae77388331
Matias Dure <497232@student.saxion.nl>
2023-02-03 13:51:00 +0100

    added js + carousel + projects + animations

---
+++ index.js
@@ -0,0 +1,64 @@
+
+//---------------------------carousel--------------------------
+//using raw JS
+let carouselParent = document.querySelector("div.carousel");
+let carouselItems = document.querySelectorAll(".carousel_item");
+
+let currentIndex = 0;
+
+let entered = false;
+let visibleItem = carouselItems[currentIndex];
+
+carouselParent.addEventListener("mouseenter", function(){
+    this.style.backgroundColor = "black";
+    this.style.background = "rgba(0, 0, 0, 0.4)";
+    applyStyleCarouselItems();
+})
+
+carouselParent.addEventListener("mouseleave", function(){
+    resetStyleCarouselItems();
+    resetStyleCarouselParent();
+})
+
+let carouselNextButton = document.getElementById("carousel_button_next");
+
+carouselNextButton.addEventListener("click", function(){
+    let index = currentIndex == carouselItems.length - 1 ? 0 : currentIndex + 1;
+    switchCarouselItem(index);
+});
+
+//using jQuery
+let carouselPrevButton = $("#carousel_button_prev");
+
+carouselPrevButton.click(function(){
+    let index = currentIndex == 0 ? carouselItems.length - 1 : currentIndex - 1;
+    switchCarouselItem(index);
+});
+
+//using raw JS
+function switchCarouselItem(index)
+{
+    resetStyleCarouselItems();
+    carouselItems[currentIndex].classList.remove("carousel_item_visible");
+    carouselItems[index].classList.add("carousel_item_visible");
+    currentIndex = index;
+    visibleItem = carouselItems[currentIndex];
+    applyStyleCarouselItems();
+}
+
+function resetStyleCarouselItems()
+{
+    visibleItem.style.width = "100%";
+}
+
+function applyStyleCarouselItems()
+{
+    visibleItem.style.width = "80%";
+    visibleItem.style.margin = "auto";
+}
+
+function resetStyleCarouselParent()
+{
+    carouselParent.style.backgroundColor = "black";
+    carouselParent.style.background = "rgba(0, 0, 0, 0)";
+}
